9cc4289cb9bb79ab4a41581ff85e5f937dd721ea
LuisDev <luisoro009@gmail.com>
2025-03-28 21:35:14 -0500

    Segundo Commit

---
+++ ecommerce/src/components/NavBar.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <nav>
+            <a href="">
+                <li>inicio</li>
+            </a><a href="">
+                <li>Contactanos</li>
+            </a><a href="">
+                <li>¿Quienes Somos?</li>
+            </a>
+        </nav>
+    </div>
+</template>
---
+++ ecommerce/src/views/Home.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import NavBar from "../components/NavBar.vue";   
+</script>
+
+<template>
+  <div class="home">
+    <NavBar/>
+    <h1 class="text-3xl font-bold text-blue-500">Bienvenidos!</h1>
+  </div>
+</template>
+
+<style scoped>
+.home{
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
